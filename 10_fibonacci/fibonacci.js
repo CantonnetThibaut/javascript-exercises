08b6038534d5a871269f2b6ee7f3f3131dd31d77
Thibaut <thcantonnet@gmail.com>
2022-11-21 18:05:40 +0100

    Exercices 05 to 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    let x = 0;
+    let y = 1;
+    for (let i = 1; i < num; i++) {
+        const temp = y;
+        y = x + y;
+        x = temp;
+    }
+    return y;
 };
 
 // Do not edit below this line
